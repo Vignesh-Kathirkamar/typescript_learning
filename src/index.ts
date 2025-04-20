7541cecda533e2449cf9126d6f804d790f5e29ee
Vignesh-Kathirkamar <vignesh.k1@lilly.com>
2025-04-20 23:40:53 +0530

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let sales: number = 123_456_789
+let course: string = "Typescript"
+let is_published: boolean = true;
+let level;
+level = "Hello";
+level = 12;
+
+function render(document: any){
+    console.log(document);
+}
