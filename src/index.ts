08f8b677f0ae8dddc91e2198c157077f9fedc6b5
Vignesh-Kathirkamar <vignesh.k1@lilly.com>
2025-04-20 23:48:08 +0530

    adding enum

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,11 @@ numbers.forEach(n=>n.toExponential)
 
 // tuples
 let user: [number, string] = [1, 'Mosh']
+
+// enums
+// list of related constants
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size {small=0, medium=1, large=2};
