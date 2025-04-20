326b061a6659dfdf9285a815720ca7928f691559
Vignesh-Kathirkamar <vignesh.k1@lilly.com>
2025-04-20 23:45:19 +0530

    added arrays and tuples

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,11 @@ level = 12;
 function render(document: any){
     console.log(document);
 }
+
+// Arrays
+
+let numbers: number[] = [1,2,3];
+numbers.forEach(n=>n.toExponential)
+
+// tuples
+let user: [number, string] = [1, 'Mosh']
